9480dad8cad528a356899b42312598262396dd2f
Evasasu <80681272+Evasasu@users.noreply.github.com>
2021-08-24 13:35:52 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCQSEhHi1fvALTok1pYBnnjCbVpvR2wW0o",
+    authDomain: "lets-chat-9689c.firebaseapp.com",
+    projectId: "lets-chat-9689c",
+    storageBucket: "lets-chat-9689c.appspot.com",
+    messagingSenderId: "374802491336",
+    appId: "1:374802491336:web:e39da288223e35852302fd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
